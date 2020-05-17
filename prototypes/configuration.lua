caebd9f7eeb97cbb3baa68582124f51b96352d44
Zombieland4942 <zombieland4942@hotmail.com>
2020-05-17 16:07:01 +1000

    Added Personal Rocket Devices

--- prototypes/configuration.lua
+++ prototypes/configuration.lua
@@ -6,40 +6,50 @@ equipment_grid = {
 }
 
 power_armor = {
-    { order = "a", name = "power-armor-mk3", equipment_grid = "xl-equipment-grid", inventory_size_bonus = 30 },
-    { order = "b", name = "power-armor-mk4", equipment_grid = "xxl-equipment-grid", inventory_size_bonus = 50 },
-    { order = "c", name = "power-armor-mk5", equipment_grid = "xxxl-equipment-grid", inventory_size_bonus = 70 },
+    { order = "a-a", name = "power-armor-mk3", equipment_grid = "xl-equipment-grid", inventory_size_bonus = 30 },
+    { order = "a-b", name = "power-armor-mk4", equipment_grid = "xxl-equipment-grid", inventory_size_bonus = 50 },
+    { order = "a-c", name = "power-armor-mk5", equipment_grid = "xxxl-equipment-grid", inventory_size_bonus = 70 },
 }
 
 fusion_reactor_equipment = {
-    { order = "d", name = "fusion-reactor-equipment-mk2", width = 4, height = 4, power = "1000kW", sprite_size = 128 },
-    { order = "e", name = "fusion-reactor-equipment-mk3", width = 4, height = 4, power = "5000kW", sprite_size = 128 },
-    { order = "f", name = "microverse-battery-mk1", width = 6, height = 6, power = "10000kW", sprite_size = 256 },
-    { order = "g", name = "microverse-battery-mk2", width = 6, height = 6, power = "20000kW", sprite_size = 256 },
+    { order = "b-a", name = "fusion-reactor-equipment-mk2", width = 4, height = 4, power = "1000kW", sprite_size = 128 },
+    { order = "b-b", name = "fusion-reactor-equipment-mk3", width = 4, height = 4, power = "5000kW", sprite_size = 128 },
+    { order = "b-c", name = "microverse-battery-mk1", width = 6, height = 6, power = "10000kW", sprite_size = 256 },
+    { order = "b-d", name = "microverse-battery-mk2", width = 6, height = 6, power = "20000kW", sprite_size = 256 },
 }
 
 personal_shield = {
-    { order = "h", name = "energy-shield-mk3-equipment", hitpoints = 200, hitpoint_recharge = 20, energy_per_hit = 40 },
-    { order = "i", name = "energy-shield-mk4-equipment", hitpoints = 300, hitpoint_recharge = 30, energy_per_hit = 50 },
+    { order = "c-a", name = "energy-shield-mk3-equipment", hitpoints = 200, hitpoint_recharge = 20, energy_per_hit = 40 },
+    { order = "c-b", name = "energy-shield-mk4-equipment", hitpoints = 300, hitpoint_recharge = 30, energy_per_hit = 50 },
 }
 
 personal_battery = {
-    { order = "j", name = "battery-mk3-equipment", capacity = "200MJ" },
-    { order = "k", name = "battery-mk4-equipment", capacity = "300MJ" },
+    { order = "d-a", name = "battery-mk3-equipment", capacity = "200MJ" },
+    { order = "d-b", name = "battery-mk4-equipment", capacity = "300MJ" },
 }
 
 personal_roboport = {
-    { order = "l", name = "personal-roboport-mk3-equipment", robots = 40, con_rad = 30, charging_station = 6, buffer = "60MJ", input = "6MW", charging = "2MW" },
-    { order = "m", name = "personal-roboport-mk4-equipment", robots = 50, con_rad = 40, charging_station = 8, buffer = "80MJ", input = "10MW", charging = "4MW" }
+    { order = "e-a", name = "personal-roboport-mk3-equipment", robots = 40, con_rad = 30, charging_station = 6, buffer = "60MJ", input = "6MW", charging = "2MW" },
+    { order = "e-b", name = "personal-roboport-mk4-equipment", robots = 50, con_rad = 40, charging_station = 8, buffer = "80MJ", input = "10MW", charging = "4MW" }
 }
 
 personal_exoskeleton = {
-    { order = "n", name = "exoskeleton-equipment-mk2", movement_bonus = 0.4, energy_consumption = "300kW" },
-    { order = "o", name = "exoskeleton-equipment-mk3", movement_bonus = 0.5, energy_consumption = "500kW" }
+    { order = "f-a", name = "exoskeleton-equipment-mk2", movement_bonus = 0.4, energy_consumption = "300kW" },
+    { order = "f-b", name = "exoskeleton-equipment-mk3", movement_bonus = 0.5, energy_consumption = "500kW" }
 }
 
 personal_laser_device = {
-    { order = "p", name = "personal-laser-defense-smg", distance = 15, damage = 0.8, cooldown = 7, energy_consumption_kJ = 50 },
-    { order = "q", name = "personal-laser-defense-assault", distance = 40, damage = 2.5, cooldown = 60, energy_consumption_kJ = 80 },
-    { order = "r", name = "personal-laser-defense-sniper", distance = 70, damage = 6, cooldown = 400, energy_consumption_kJ = 400 },
+    { order = "g-a", name = "personal-laser-defense-smg", distance = 15, damage = 0.8, cooldown = 7, energy_consumption_kJ = 50 },
+    { order = "g-b", name = "personal-laser-defense-assault", distance = 40, damage = 2.5, cooldown = 60, energy_consumption_kJ = 80 },
+    { order = "g-c", name = "personal-laser-defense-sniper", distance = 70, damage = 6, cooldown = 400, energy_consumption_kJ = 400 },
+}
+
+personal_canno_projectile = {
+    { name = "pcd-projectile-1", damage = 2000, radius = 5, acceleration = 0.05 },
+    { name = "pcd-projectile-2", damage = 20000, radius = 10, acceleration = 0.05 }
+}
+
+personal_cannon_device = {
+    { order = "h-a", name = "personal-cannon-defense-mk1", projectile = "pcd-projectile-1", distance = 70, cooldown = 30, energy = "1MJ" },
+    { order = "h-b", name = "personal-cannon-defense-mk2", projectile = "pcd-projectile-2", distance = 90, cooldown = 30, energy = "10MJ" }
 }
---
+++ prototypes/personal_cannon.lua
@@ -0,0 +1,182 @@
+
+for x,projectile in pairs(personal_canno_projectile) do
+    data:extend(
+        {
+            {
+                type = "projectile",
+                name = projectile.name,
+                flags = {"not-on-map"},
+                acceleration = projectile.acceleration,
+                action =
+                {
+                    type = "direct",
+                    action_delivery =
+                    {
+                        type = "instant",
+                        target_effects =
+                        {
+                            {
+                                type = "create-entity",
+                                entity_name = "big-explosion"
+                            },
+                            {
+                                type = "damage",
+                                damage = {amount = projectile.damage, type = "explosion"}
+                            },
+                            {
+                                type = "create-entity",
+                                entity_name = "medium-scorchmark-tintable",
+                                check_buildability = true
+                            },
+                            {
+                                type = "invoke-tile-trigger",
+                                repeat_count = 1,
+                            },
+                            {
+                                type = "destroy-decoratives",
+                                from_render_layer = "decorative",
+                                to_render_layer = "object",
+                                include_soft_decoratives = true,
+                                include_decals = false,
+                                invoke_decorative_trigger = true,
+                                decoratives_with_trigger_only = false,
+                                radius = 5
+                            },
+                            {
+                                type = "nested-result",
+                                action =
+                                {
+                                    type = "area",
+                                    radius = projectile.radius,
+                                    action_delivery =
+                                    {
+                                        type = "instant",
+                                        target_effects =
+                                        {
+                                            {
+                                                type = "damage",
+                                                damage = {amount = projectile.damage, type = "explosion"}
+                                            },
+                                            {
+                                                type = "create-entity",
+                                                entity_name = "explosion"
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                light = {intensity = 0.5, size = 4},
+                animation =
+                {
+                    filename = "__base__/graphics/entity/rocket/rocket.png",
+                    frame_count = 8,
+                    line_length = 8,
+                    width = 9,
+                    height = 35,
+                    shift = {0, 0},
+                    priority = "high"
+                },
+                shadow =
+                {
+                    filename = "__base__/graphics/entity/rocket/rocket-shadow.png",
+                    frame_count = 1,
+                    width = 7,
+                    height = 24,
+                    priority = "high",
+                    shift = {0, 0}
+                },
+                smoke =
+                {
+                    {
+                        name = "smoke-fast",
+                        deviation = {0.15, 0.15},
+                        frequency = 1,
+                        position = {0, 1},
+                        slow_down_factor = 1,
+                        starting_frame = 3,
+                        starting_frame_deviation = 5,
+                        starting_frame_speed = 0,
+                        starting_frame_speed_deviation = 5
+                    }
+                }
+            },
+        }
+    )
+end
+
+
+for x,pcd in pairs(personal_cannon_device) do
+    data:extend(
+        {
+            {
+                type = "item",
+                name = pcd.name,
+                icon = "__darkstar-equipment__/graphics/icons/" .. pcd.name .. ".png",
+                icon_size = 64, icon_mipmaps = 4,
+                placed_as_equipment_result = pcd.name,
+                subgroup = "ds-equipment",
+                order = pcd.order,
+                default_request_amount = 5,
+                stack_size = 20
+            },
+            {
+                type = "active-defense-equipment",
+                name = pcd.name,
+                sprite =
+                {
+                    filename = "__darkstar-equipment__/graphics/equipment/" .. pcd.name .. ".png",
+                    width = 351,
+                    height = 516,
+                    priority = "medium"
+                },
+                shape =
+                {
+                    width = 8,
+                    height = 16,
+                    type = "full"
+                },
+                energy_source =
+                {
+                    type = "electric",
+                    usage_priority = "secondary-input",
+                    buffer_capacity = "10MJ"
+                },
+                attack_parameters =
+                {
+                    type = "projectile",
+                    ammo_category = "electric",
+                    cooldown = pcd.cooldown,
+                    damage_modifier = 0.5,
+                    range = pcd.distance,
+                    ammo_type =
+                    {
+                        type = "projectile",
+                        category = "electric",
+                        energy_consumption = "1MJ",
+                        projectile = pcd.projectile,
+                        speed = 1,
+                        action =
+                        {
+                            {
+                                type = "direct",
+                                action_delivery =
+                                {
+                                    {
+                                        type = "projectile",
+                                        projectile = pcd.projectile,
+                                        starting_speed = 1.28
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                automatic = true,
+                categories = {"armor"}
+            },
+        }
+    )
+end
